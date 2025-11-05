79ed55f4833b023b6a885be129114e269fea09be
JavierAparicio777 <ernestohedificio@gmail.com>
2025-11-05 19:55:18 +0100

    Mensaje del commit

---
+++ Proyecto-login/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <a href="login.php">¿Tienes cuenta ya?</a>
+    <a href="registro.php">¿No tienes cuenta todavía?</a>
+</body>
+</html>
